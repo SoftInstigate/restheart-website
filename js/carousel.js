c0e5a12af23d5c69011a9473caa5553f95390754
Maurizio Turatti <maurizio@softinstigate.com>
2019-01-29 18:49:38 +0100

    Add breakpoint 992 to carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,23 +1,29 @@
-$(document).ready(function(){
-    console.log("piupp");
-    $('.customer-logos').slick({
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1000,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      responsive: [{
-        breakpoint: 768,
+$(document).ready(function () {
+  console.log("piupp");
+  $('.customer-logos').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+        breakpoint: 992,
         settings: {
           slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
         }
       }, {
         breakpoint: 520,
         settings: {
-          slidesToShow: 3
+          slidesToShow: 2
         }
       }]
-    });
   });
+});
